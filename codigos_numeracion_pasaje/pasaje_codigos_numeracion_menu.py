4ad5ee199a248bbe6e2b852cf7703fa5115857e5
Francisco Chiminelli <67705754+x-chama-x@users.noreply.github.com>
2024-08-03 00:01:41 -0300

    Add files via upload

--- codigos_numeracion_pasaje/pasaje_codigos_numeracion_menu.py
+++ codigos_numeracion_pasaje/pasaje_codigos_numeracion_menu.py
@@ -1,7 +1,11 @@
-# programa que convierte un número decimal a los códigos BCD, Gray y Johnson
-
 import sys
+import os
 
+# Función para limpiar la pantalla
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Funciones de conversión
 def decimal_to_bcd(decimal):
     return ''.join(format(int(d), '04b') for d in str(decimal))
 
@@ -63,11 +67,103 @@ def decimal_to_johnson(decimal, bits):
     else:
         return '1' * (2 * bits - decimal) + '0' * (decimal - bits)
 
+
+def calcular_exponente_real(num):
+    binary_representation = decimal_a_binario(num)
+    punto_decimal = binary_representation.index('.')
+    first_one_index = binary_representation.index('1')
+    if first_one_index < punto_decimal:
+        return punto_decimal - first_one_index - 1
+    else:
+        return punto_decimal - first_one_index
+
+def normalizar_binario(num):
+    binary_representation = decimal_a_binario(num)
+    punto_decimal = binary_representation.index('.')
+    first_one_index = binary_representation.index('1')
+    
+    normalized = binary_representation.replace('.', '')
+    normalized = normalized[first_one_index:] + '0' * first_one_index
+    normalized = normalized[:1] + '.' + normalized[1:]
+    
+    return normalized
+
+def obtener_mantisa(binario_normalizado):
+    fraccionaria = binario_normalizado.split('.')[1]
+    mantisa = fraccionaria.ljust(23, '0')[:23]
+    return mantisa
+
+def calcular_exponente(num):
+    exponente_real = calcular_exponente_real(num)
+    exponente = exponente_real + 127
+    return exponente
+
+def determinar_bit_de_signo(num):
+    return 1 if num < 0 else 0
+
+def decimal_a_binario(num):
+    if num == 0:
+        return "0.0"
+    
+    signo = '-' if num < 0 else ''
+    num = abs(num)
+    entero, decimal = str(num).split('.')
+    entero = int(entero)
+    decimal = float("0." + decimal)
+    
+    binario_entero = bin(entero)[2:]
+    
+    binario_decimal = ""
+    precision = 23
+    while decimal > 0 and len(binario_decimal) < precision:
+        decimal *= 2
+        if decimal >= 1:
+            binario_decimal += "1"
+            decimal -= 1
+        else:
+            binario_decimal += "0"
+    
+    return f"{signo}{binario_entero}.{binario_decimal}"
+
+def obtener_punto_flotante_IEEE754(num):
+    signo = determinar_bit_de_signo(num)
+    exponente = calcular_exponente(num)
+    mantisa = obtener_mantisa(normalizar_binario(num))
+    exponente_binario = f"{exponente:08b}"
+    punto_flotante_binario = f"{signo}{exponente_binario}{mantisa}"
+    return punto_flotante_binario
+
+def imprimir_representaciones(binario):
+    octal = oct(int(binario, 2))
+    hexadecimal = hex(int(binario, 2))
+    print("\nRepresentaciones en norma IEEE754 :")
+    print(f"Binario: {binario}")
+    print(f"Octal: {octal}")
+    print(f"Hexadecimal: {hexadecimal}")
+
+def ieee754_to_decimal(ieee754_str):
+    sign = int(ieee754_str[0])
+    exponent = int(ieee754_str[1:9], 2)
+    fraction = ieee754_str[9:]
+
+    real_exponent = exponent - 127
+
+    decimal_fraction = 1.0
+    for i, bit in enumerate(fraction):
+        if bit == '1':
+            decimal_fraction += 2 ** -(i + 1)
+
+    result = (-1) ** sign * decimal_fraction * (2 ** real_exponent)
+
+    return result
+
+# Funciones de menú
 def bcd_conversion():
     while True:
         try:
             decimal = input("\nIngrese un número decimal para convertir a BCD (o 'q' para volver al menú principal): ")
             if decimal.lower() == 'q':
+                clear_screen()
                 break
             decimal = int(decimal)
             if decimal < 0:
@@ -81,6 +177,7 @@ def gray_conversion():
         try:
             bits = input("\nIngrese la cantidad de bits para el código Gray (o 'q' para volver al menú principal): ")
             if bits.lower() == 'q':
+                clear_screen()
                 break
             bits = int(bits)
             if bits <= 0:
@@ -101,6 +198,7 @@ def johnson_conversion():
         try:
             bits = input("\nIngrese la cantidad de bits para el código Johnson (o 'q' para volver al menú principal): ")
             if bits.lower() == 'q':
+                clear_screen()
                 break
             bits = int(bits)
             if bits <= 0:
@@ -116,15 +214,60 @@ def johnson_conversion():
         except ValueError as e:
             print(f"Error: {e}. Por favor, ingrese valores válidos.")
 
+def decimal_to_ieee754_conversion():
+    while True:
+        try:
+            num = input("\nIngrese un número decimal para convertir a IEEE754 (o 'q' para volver al menú principal): ")
+            if num.lower() == 'q':
+                clear_screen()
+                break
+            num = float(num)
+            
+            binario_normalizado = normalizar_binario(num)
+            mantisa = obtener_mantisa(binario_normalizado)
+            exponente = calcular_exponente(num)
+            signo = determinar_bit_de_signo(num)
+            punto_flotante = obtener_punto_flotante_IEEE754(num)
+
+            print("\nDatos del número en punto flotante:")
+            print(f'Número ingresado: {num}')
+            print(f'Representación binaria: {decimal_a_binario(num)}')
+            print(f'Binario normalizado: {binario_normalizado}')
+            print(f'Mantisa: {mantisa}')
+            print(f'Exponente: {exponente}')
+            print(f'Bit de signo: {signo}')
+            imprimir_representaciones(punto_flotante)
+        except ValueError as e:
+            print(f"Error: {e}. Por favor, ingrese un número decimal válido.")
+
+def ieee754_to_decimal_conversion():
+    while True:
+        print("\nConversor de IEEE 754 de 32 bits a decimal")
+        ieee754_input = input("Ingrese el número en formato binario (32 bits) (o 'q' para volver al menú principal): ")
+        
+        if ieee754_input.lower() == 'q':
+            clear_screen()
+            break
+
+        if len(ieee754_input) != 32 or not all(bit in '01' for bit in ieee754_input):
+            print("Error: Por favor ingrese un número binario de 32 bits válido.")
+            continue
+
+        decimal_result = ieee754_to_decimal(ieee754_input)
+        print(f"\nEl número decimal equivalente es: {decimal_result}")
+
 def main_menu():
+    clear_screen()
     while True:
         print("\n--- Menú Principal ---")
         print("1. Conversión a códigos BCD")
         print("2. Conversión a código Gray")
         print("3. Conversión a código Johnson")
-        print("4. Salir")
+        print("4. Conversión de decimal a IEEE754")
+        print("5. Conversión de IEEE754 a decimal")
+        print("6. Salir")
         
-        choice = input("Seleccione una opción (1-4): ")
+        choice = input("Seleccione una opción (1-6): ")
         
         if choice == '1':
             bcd_conversion()
@@ -133,10 +276,14 @@ def main_menu():
         elif choice == '3':
             johnson_conversion()
         elif choice == '4':
+            decimal_to_ieee754_conversion()
+        elif choice == '5':
+            ieee754_to_decimal_conversion()
+        elif choice == '6':
             print("Gracias por usar el programa. ¡Hasta luego!")
             sys.exit(0)
         else:
-            print("Opción no válida. Por favor, seleccione una opción del 1 al 4.")
+            print("Opción no válida. Por favor, seleccione una opción del 1 al 6.")
 
 if __name__ == "__main__":
     print("Bienvenido al programa de conversión de códigos")
